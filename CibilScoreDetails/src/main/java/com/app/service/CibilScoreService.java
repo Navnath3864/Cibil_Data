dc152656662c1796e22ad480f9158074c27f1d65
DELL-PC <DELL-PC@PC>
2025-01-20 16:25:24 +0530

    Add post and getAll method by harshada

--- CibilScoreDetails/src/main/java/com/app/service/CibilScoreService.java
+++ CibilScoreDetails/src/main/java/com/app/service/CibilScoreService.java
@@ -1,5 +1,13 @@
 package com.app.service;
 
+import java.util.List;
+
+import com.app.model.CibilScoreData;
+
 public interface CibilScoreService {
+
+public	CibilScoreData addCibilScore(CibilScoreData cs);
+
+public List<CibilScoreData> viewAllCibilScores(List<CibilScoreData> cs);
 
 }
